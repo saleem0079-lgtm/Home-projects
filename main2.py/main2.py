ee076910818cc23da5f074ca2a9485f7d62fa0aa
saleem0079-lgtm <saleem0079@gmail.com>
2025-09-26 17:05:34 -0500

    save

---
+++ main2.py/main2.py
@@ -0,0 +1,6 @@
+hot=int(input("Enter your tempreture hot"))
+cold=int(input("Enter your tempreture cold"))
+if hot>cold:
+    print("Don't where A jacket today")
+elif cold>hot:
+    print("Where a jacket")
